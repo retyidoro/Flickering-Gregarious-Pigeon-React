96c8d0fe808108b6c1be46c7deb4513ed45b058e
Dorottya Ferencz <retyidoro@yahoo.com>
2023-05-29 12:50:32 +0300

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -17,10 +17,11 @@ const Home = (props) => {
         src="/aaa/screenshot%202023-04-28%20at%2011.04.05-200w.png"
         className="home-image"
       />
+      <img alt="image" src="/aaa/airplane-200h.jpg" className="home-image1" />
       <img
-        src="/aaa/favicon-32x32-200h.png"
         alt="image"
-        className="home-image1"
+        src="/aaa/hibiscus-fav2-200h.png"
+        className="home-image2"
       />
     </div>
   )
