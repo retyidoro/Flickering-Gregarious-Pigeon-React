b17b7bf4597533cdc8f1bdaad51dc0511fadaaba
Dorottya Ferencz <retyidoro@yahoo.com>
2023-05-29 12:39:37 +0300

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Flickering Gregarious Pigeon</title>
+        <meta property="og:title" content="Flickering Gregarious Pigeon" />
+      </Helmet>
+      <h1>Heading</h1>
+      <img
+        alt="image"
+        src="/aaa/screenshot%202023-04-28%20at%2011.04.05-200w.png"
+        className="home-image"
+      />
+    </div>
+  )
+}
+
+export default Home
